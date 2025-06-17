c5b552f4c8d9e3dfe8abcdb1b677422040f54a01
MichaelKudron <kobrys87@gmail.com>
2025-06-18 00:39:17 +0200

    another fix (let me die)

---
+++ src/LuxPakierna.Api/LuxPakiernia.Infrastructure/Migrations/20250617223649_exerciseInPlanv2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxPakiernia.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class exerciseInPlanv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
